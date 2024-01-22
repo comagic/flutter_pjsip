20feba8b6f240dacff5c93afaf4b6c4790ccdc57
nesquikm <drapolyuk@gmail.com>
2024-01-22 14:15:30 +0400

    feat: add SipEventBody and TxErrorEvent classes

--- packages/flutter_pjsip/lib/pjsua/siptypes/sip_event_body.dart
+++ packages/flutter_pjsip/lib/pjsua/siptypes/sip_event_body.dart
@@ -1,1 +1,28 @@
-// TODO(nesquikm): SipEventBody
+import 'package:flutter_pjsip/pjsua/pjsua.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'sip_event_body.freezed.dart';
+
+/// The event body.
+@freezed
+sealed class SipEventBody with _$SipEventBody {
+  const factory SipEventBody({
+    /// Timer event.
+    required TimerEvent timer,
+
+    /// Transaction state has changed event.
+    required TsxStateEvent tsxState,
+
+    /// Message transmission event.
+    required TxMsgEvent txMsg,
+
+    /// Transmission error event.
+    required TxErrorEvent txError,
+
+    /// Message arrival event.
+    required RxMsgEvent rxMsg,
+
+    /// User event.
+    required UserEvent user,
+  }) = _SipEventBody;
+}

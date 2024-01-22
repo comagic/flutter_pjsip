4e298ec6c23f18794929a72e2fc353ba2e5bd5cd
nesquikm <drapolyuk@gmail.com>
2024-01-22 13:37:18 +0400

    feat: add PjsipEventIdE and TsxStateEvent classes

--- packages/flutter_pjsip/lib/pjsua/siptypes/tsx_state_event.dart
+++ packages/flutter_pjsip/lib/pjsua/siptypes/tsx_state_event.dart
@@ -1,1 +1,27 @@
-// TODO(nesquikm): TsxStateEvent
+import 'package:flutter_pjsip/pjsua/pjsua.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'tsx_state_event.freezed.dart';
+
+/// This structure describes transaction state event source.
+@freezed
+sealed class TsxStateEvent with _$TsxStateEvent {
+  const factory TsxStateEvent({
+    /// Event source.
+    required TsxStateEventSrc src,
+
+    /// The transaction.
+    required SipTransaction tsx,
+
+    /// Previous state.
+    required PjsipTsxStateE prevState,
+
+    /// Type of event source:
+    /// - PJSIP_EVENT_TX_MSG
+    /// - PJSIP_EVENT_RX_MSG,
+    /// - PJSIP_EVENT_TRANSPORT_ERROR
+    /// - PJSIP_EVENT_TIMER
+    /// - PJSIP_EVENT_USER
+    required PjsipEventIdE type,
+  }) = _TsxStateEvent;
+}
